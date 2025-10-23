77951c9a6da528885e3e9ed409a0bb4b6a1a9d1c
Abdurahmon Erkinjonov <abdirahmon31@gmail.com>
2025-10-23 17:25:45 +0500

    .env qo'shildi

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from environs import Env
+
 import asyncio
+
+env = Env()
+env.read_env()
 
 logging.basicConfig(level=logging.INFO)
 
-BOT_TOKEN = "8244607659:AAHs_RU3fjnCINY3nh-OjVevq1Tc_vrJDAI"
+BOT_TOKEN = env.str("BOT_TOKEN")
+
 CHANNEL_ID = -1003174286262 
 
 # Bot va Dispatcher yaratish
